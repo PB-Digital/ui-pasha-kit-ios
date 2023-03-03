7df538466e55221fd4ee5973b0cd81ca1f474bb8
Farid Valiyev <91952345+faridpashabank@users.noreply.github.com>
2023-03-03 15:50:03 +0400

    Uİ PashaKit repo first commit
    This UI Kit was created by Pashabank

---
+++ Sources/PashaKit/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  UIColor+Extensions.swift
+//  
+//
+//  Created by Murad on 10.12.22.
+//
+//
+//  MIT License
+//
+//  Copyright (c) 2022 Murad Abbasov
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+
+extension UIColor {
+    struct Colors {
+        static let PBGreen = UIColor(red: 0.11, green: 0.678, blue: 0.573, alpha: 1)
+        static let PBGreenCardInput = UIColor(red: 0.043, green: 0.29, blue: 0.247, alpha: 1)
+        static let PBGreenSecondary = UIColor(red: 0.011, green: 0.68, blue: 0.491, alpha: 0.08)
+        static let PBGray = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+        static let PBBlackMedium = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6)
+        static let PBGraySecondary = UIColor(red: 0.883, green: 0.883, blue: 0.883, alpha: 1)
+        static let PBGrayTransparent = UIColor(red: 0.604, green: 0.608, blue: 0.612, alpha: 0.08)
+        static let PBFauxChestnut = UIColor(red: 0.6, green: 0.325, blue: 0.188, alpha: 1)
+        static let PBRed = UIColor(red: 0.804, green: 0.094, blue: 0.09, alpha: 1)
+        static let PBRed8 = UIColor(red: 0.925, green: 0.18, blue: 0.141, alpha: 0.08)
+        static let PBInvalidRed = UIColor(red: 0.906, green: 0.322, blue: 0.251, alpha: 1)
+    }
+}
